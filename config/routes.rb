0a6c991485f32a91c024c3d13bfb26bd2a8d36e2
Eric Lacaille <elacaille0@gmail.com>
2016-05-10 11:29:34 +0200

    Gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # list all the task
+  get '/tasks' , to: 'tasks#index'
+  # display one task
+  # create new
+  # edit update
+  # delete
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
